237a4ef3f00cb269242f480d2db3454d478520e6
Hector Subiri <hg@Hectors-MacBook-Air.local>
2026-05-25 22:33:41 -0600

    feat: initial project setup with status controller

---
+++ src/main/java/com/hector/portfolio_base/PortfolioBaseApplication.java
@@ -0,0 +1,17 @@
+package com.hector.portfolio_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class PortfolioBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBaseApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/hector/portfolio_base/PortfolioBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hector.portfolio_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
